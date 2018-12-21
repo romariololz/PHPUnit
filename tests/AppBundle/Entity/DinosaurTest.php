17b2c026ed22390f44edc035bd6cc6bab10beec7
Romain Helou <helouromain@gmail.com>
2018-12-21 15:03:45 +0100

    1 - PHPUnit: Secure the Park

---
+++ tests/AppBundle/Entity/DinosaurTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romariololz
+ * Date: 21/12/18
+ * Time: 14:59
+ */
+
+namespace Tests\AppBundle\Entity;
+
+
+use PHPUnit\Framework\TestCase;
+
+class DinosaurTest extends TestCase
+{
+
+}
