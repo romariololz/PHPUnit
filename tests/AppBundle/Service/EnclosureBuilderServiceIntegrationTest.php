7c49be60ba635c089f5cebac81ba9c65868f4505
Romain Helou <helouromain@gmail.com>
2018-12-22 01:19:07 +0100

    21 - Clearing the Database

--- tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceIntegrationTest.php
@@ -10,17 +10,24 @@ namespace Tests\AppBundle\Service;
 
 
 use AppBundle\Entity\Dinosaur;
+use AppBundle\Entity\Enclosure;
 use AppBundle\Entity\Security;
 use AppBundle\Service\EnclosureBuilderService;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
 {
-    public function testItBuildsEnclosureWithDefaultSpecification()
+    protected function setUp()
     {
         self::bootKernel();
 
+        $this->truncateEntities();
+    }
+
+    public function testItBuildsEnclosureWithDefaultSpecification()
+    {
         /** @var EnclosureBuilderService $enclosureBuilderService */
         $enclosureBuilderService = self::$kernel->getContainer()
             ->get('test.'.EnclosureBuilderService::class);
@@ -28,9 +35,7 @@ class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
         $enclosureBuilderService->buildEnclosure();
 
         /** @var EntityManager $em */
-        $em = self::$kernel->getContainer()
-            ->get('doctrine')
-            ->getManager();
+        $em = $this->getEntityManager();
 
         $count = (int) $em->getRepository(Security::class)
             ->createQueryBuilder('s')
@@ -47,5 +52,22 @@ class EnclosureBuilderServiceIntegrationTest extends KernelTestCase
             ->getSingleScalarResult();
 
         $this->assertSame(3, $count, 'Amount of security systems is not the same');
+    }
+
+    private function truncateEntities()
+    {
+        $purger = new ORMPurger($this->getEntityManager());
+        $purger->purge();
+    }
+
+    /**
+     * @return EntityManager
+     */
+    private function getEntityManager()
+    {
+        /** @var EntityManager $em */
+        return self::$kernel->getContainer()
+            ->get('doctrine')
+            ->getManager();
     }
 }
